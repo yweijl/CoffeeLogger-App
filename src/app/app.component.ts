845b72fd39c8b98225fb22aae8076210c706d365
Yoshua Weijl <y.weijl@rdc.nl>
2020-11-06 09:39:17 +0100

    Added initial coffeecard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CoffeeLogger';
+  cardImage = '../assets/Logos/IefIdo_CMYK_V2-Hersteld_Rondo-Mexico.png'
+  rate = 4;
+  
+  constructor(private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer){
+      this.registerIcon('Icons','portafilter');
+      this.registerIcon('Icons','information');
+      this.registerIcon('Icons','beans');
+      this.registerIcon('Icons','espresso');
+      this.registerIcon('Icons','country');
+    }
+
+  private registerIcon(path: string, icon: string){
+    this.matIconRegistry.addSvgIcon(
+      icon,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(`../assets/${path}/${icon}.svg`)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material components
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { HttpClientModule } from "@angular/common/http";
+
+//npm components
+import { BarRatingModule } from "ngx-bar-rating";
 
 @NgModule({
   declarations: [
@@ -10,7 +20,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
+    // material components
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    
+    // npm components
+    BarRatingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
